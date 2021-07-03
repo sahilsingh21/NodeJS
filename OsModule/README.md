782926f3ec4cf76e640355f7cb6259b7c677f0ae
sahilsingh21 <ss3842486@gmail.com>
2021-07-03 20:13:14 +0530

    Operating system get info in this file and website

---
+++ OsModule/README.md
@@ -0,0 +1,5 @@
+# NodeJS OS
+
+https://nodejs.org/dist/latest-v14.x/docs/api/os.html
+
+All function in website
---
+++ OsModule/osmod.js
@@ -0,0 +1,5 @@
+//  https://nodejs.org/dist/latest-v14.x/docs/api/os.html
+
+const os = require("os");
+
+console.log(os.version());
