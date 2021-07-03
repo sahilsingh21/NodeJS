735c8858fa3d0c01046f2f3886119cb499c059e9
sahilsingh21 <ss3842486@gmail.com>
2021-07-03 19:58:47 +0530

    read write delete append in Async

--- ReadWriteAsync/index.js
+++ ReadWriteAsync/index.js
@@ -2,20 +2,29 @@
 
 const fs = require("fs");
 
+fs.mkdir("task",(err)=>{
+    console.log("directory will be created");
+})
+
 // we creating the file in asyncronase way that's why we need callback
-fs.writeFile("read.txt","Hello Ji", (err)=>{
+fs.writeFile("./task/read.txt","Hello Ji", (err)=>{
     console.log("Your task is complteted");
     console.log(err);
 })
 
 // append data in async and callback
-fs.appendFile("read.txt"," How are you ?", (err)=>{
+fs.appendFile("./task/read.txt", " How are you ?", (err)=>{
     console.log("You have appended the data");
     console.log(err);
 })
 
 // read data in async with callback, but in call back two argument will pass first is eroor and second to stode data 
-fs.readFile("read.txt","utf-8",(err, data)=>{
+fs.readFile("./task/read.txt","utf-8",(err, data)=>{
     console.log(data);
     console.log(err);
+})
+
+//deleting file in async
+fs.unlink("./task/read.txt",(err)=>{
+    console.log("File is delete");
 })
