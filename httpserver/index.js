87275510bb6b261c195bb95f3aab4c11bd15e705
sahilsingh21 <ss3842486@gmail.com>
2021-07-04 00:40:58 +0530

    Creating api to get data and show to the server webpage

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,14 +1,21 @@
 const http = require("http");
-
+const fs = require("fs");
 // creating server and store it to server variable
 const server = http.createServer((req, res)=>{
 
     // getting url from req.url
     // console.log(req.url); 
+    const data = fs.readFileSync(`${__dirname}/UserApi/userapi.json`,"utf-8")
+
+    const objData = JSON.parse(data);
 
     //localhost home page
     if(req.url == "/"){
         res.end(" This is Home Page ");
+    }else if(req.url == "/userapi"){   // User API page
+
+    res.end(objData[3].name);
+
     }else if(req.url == "/about"){   // about page
         res.end("This is about page");
     } else{ // any page 
