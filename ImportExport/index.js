dfe63b0dc2cfe968819b6ae444eede2cf5a720ae
sahilsingh21 <ss3842486@gmail.com>
2021-07-03 21:13:37 +0530

    Import and Export to one file to another in NodeJS

---
+++ ImportExport/index.js
@@ -0,0 +1,18 @@
+// const oper = require("./oper");
+
+// console.log(oper);
+
+// console.log(oper.add(5,6));
+
+// console.log(oper.sub(12,6));
+
+const {add, sub, mult, name} = require("./oper");
+
+
+console.log(add(5,6));
+
+console.log(sub(12,6));
+
+console.log(mult(12,6));
+
+console.log(name);
---
+++ ImportExport/oper.js
@@ -0,0 +1,19 @@
+const add = (a, b)=>{
+ return a+b;
+}
+
+const  sub = (a,b) =>{
+    return a-b;
+}
+
+const  mult = (a,b) =>{
+    return a*b;
+}
+
+const name = "SINGH";
+
+// module.exports.add = add;
+// module.exports.sub = sub;
+// module.exports.mult = mult;
+
+module.exports = {add, sub, mult, name};
