6a4bc12157c9e8904343a791ae84cae08a06281b
sahilsingh21 <ss3842486@gmail.com>
2021-07-03 23:29:38 +0530

    routing method only use of if else statment with help of request and response

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,9 +1,23 @@
 const http = require("http");
 
+// creating server and store it to server variable
 const server = http.createServer((req, res)=>{
-    res.end("Hello Mr. ");
+
+    // getting url from req.url
+    // console.log(req.url); 
+
+    //localhost home page
+    if(req.url == "/"){
+        res.end(" This is Home Page ");
+    }else if(req.url == "/about"){   // about page
+        res.end("This is about page");
+    } else{ // any page 
+        res.writeHead(404, {"content-type": "text/html"}); // Adding status code 404 if page is not found
+        res.end("<h1>404 ERROR PAGE<h1>");
+    }
 });
 
+// server listining to port number 8000
 server.listen(8000, "localhost",()=>{
     console.log("we are listening to port number 8000");
 })
