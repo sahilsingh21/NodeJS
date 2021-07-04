af94eebb7db0759bfaf2b9705b5a3f7cb074f4d5
sahilsingh21 <ss3842486@gmail.com>
2021-07-04 17:05:04 +0530

    Stream Pipe method only one of code

--- StreamModule/index.js
+++ StreamModule/index.js
@@ -5,6 +5,7 @@ const http = require("http");
 const server = http.createServer();
 
 server.on("request",(req,res)=>{
+    // 1st way
     // fs.readFile("read.txt", (err, data)=>{
     //     if(err){
     //         return console.log(err);
@@ -13,24 +14,29 @@ server.on("request",(req,res)=>{
     //     }
     // })
 
-    // read streaming by fs module  
+//     //2nd way
+//     // read streaming by fs module  
+//     const rstream = fs.createReadStream("read.txt");
+
+// //  to get data chunk by chunk and write to web page
+//     rstream.on("data", (chunk)=>{
+//         res.write(chunk);
+//     })
+
+// // when all data is read then we should end it
+//     rstream.on("end",()=>{
+//         res.end();
+//     })
+
+//     // if file is not present it throughs error
+//     rstream.on("error",(err)=>{
+//         console.log(err);
+//         res.end("File is not thier");
+//     })
+
+// 3rd way
     const rstream = fs.createReadStream("read.txt");
-
-//  to get data chunk by chunk and write to web page
-    rstream.on("data", (chunk)=>{
-        res.write(chunk);
-    })
-
-// when all data is read then we should end it
-    rstream.on("end",()=>{
-        res.end();
-    })
-
-    // if file is not present it throughs error
-    rstream.on("error",(err)=>{
-        console.log(err);
-        res.end("File is not thier");
-    })
+    rstream.pipe(res);
 
 })
 
